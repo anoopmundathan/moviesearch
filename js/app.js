a2f63b74c6cb24fc9a08c795657b6d009c2f5a5d
anoopmundathan <anoop.mundathan@gmail.com>
2016-09-22 23:52:28 +0100

    Added imdb button and functionality

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
 
+	// Handle keyboard events 
 	$('input').keyup(function() {
+
+		// Hide Movie description page and show main page
+		$('.movie-plot-header').hide();
+		$('.poster').remove();
+		$('#movies').show();
+
 		searchMovies();
 	})
 
+    // Search Movies - Perform ajax request
 	function searchMovies() {
+
+		// Retrieve the user input
 		var movieTitle = $('#search').val().toLowerCase();
-		var movieYear = $('#year').val().toLowerCase();
-		var innerHTML = "";
+		var movieYear  = $('#year').val().toLowerCase();
+		var innerHTML  = "";
 		
 		// Request data from the OMDb API to display movie information
 		$.ajax({
 
-			url: "http://www.omdbapi.com/?s=" + movieTitle + "&y=" + movieYear + "&r=json",
+			url: "https://www.omdbapi.com/?s=" + movieTitle + "&y=" + movieYear + "&r=json",
 			method: "GET",
 			dataType: 'json',
 			success: function(data) { // If ajax request is succesful display movie information
@@ -31,7 +41,7 @@ $(document).ready(function() {
 					}); // End each method
 
 
-				} else if(data.Response === "False") {
+				} else if(data.Response === "False") { // If no movies found
 					innerHTML += "<li class='no-movies'>";
 					innerHTML += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + movieTitle ;
 					innerHTML += "</li>";
@@ -44,42 +54,50 @@ $(document).ready(function() {
 					$('li').click(function(e) {
 						e.preventDefault();
 						
-						// Make ajax request 
+						// Make ajax request to get movie description
 						$.ajax({
-							url: "http://www.omdbapi.com/?i=" + $(this).attr('id') + "&plot=full&r=json",
+							url: "https://www.omdbapi.com/?i=" + $(this).attr('id') + "&plot=full&r=json",
 							method: "GET",
 							dataType: 'json',
 
 							success: function(data) {
+			
+								var imdbURL  = 'http://www.imdb.com/title/' + data.imdbID + '/';
 								$('#movies').hide();
+								$('.movie-button').unbind('click'); // Remove button click event 
 								
 								// Generate dynamic content 
 								$('.movie-plot-poster').append("<img class='poster' src='" + data.Poster + "'>");
 								$('.movie-plot-title').html('<h1>' + data.Title + '</h1><p>IMDB Rating : ' + data.imdbRating + '</p>');
-								$('.movie-plot-story').html('<p>' + data.Plot + '</p');
+								$('.movie-plot-story').html('<h3>Plot Synopsis</h3><p>' + data.Plot + '</p');
 
 								$('.movie-plot-header').show();
 
-								// Go back to search results when clicked
+								// Go back to search results page 
 								$('.search-result').click(function(e) {
 									e.preventDefault();
 									$('.movie-plot-header').hide();
 									$('.poster').remove();
 									$('#movies').show();
-									console.log('hello');
 								});
 
+								// Open imdb site in a new window tab
+								$('.movie-button').click(function() {
+									//window.location.href = 'http://www.imdb.com/title/' + data.imdbID + '/';
+									window.open(imdbURL, '_blank');
+								})
 
 							} // End success function for movie description
 
 						}); // End ajax method for plot
 
-					}); // End li
-				}
+					}); // End li click
+				} // End if condition
 
 			} // End success function
-		}); // End ajax method
-	} // End searchMovies function
 
+		}); // End ajax method
+
+	} // End searchMovies function
 
 }); // End document.ready
